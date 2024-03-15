6ae088031ff676c67eb747638b0f0653c6d85a17
Alok kumar meena <123947718+Alok-meena@users.noreply.github.com>
2024-03-15 19:23:25 +0530

    Update reversequeue.cpp

--- dsa/reversequeue.cpp
+++ dsa/reversequeue.cpp
@@ -1,1 +1,21 @@
-
+class Solution
+{
+    private:
+    void reverse(queue<int>&q){
+        if(q.empty()){
+            
+            return;
+        }
+        int d=q.front();
+        q.pop();
+        reverse(q);
+        q.push(d);
+    }
+    
+    public:
+    queue<int> rev(queue<int> q)
+    {
+    reverse(q);
+    return q;
+    }
+};
